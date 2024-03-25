f6ffc5464d7d0a414dd62ae01fd6411da1b64f34
Graeme Gillies <graeme.a.gillies@gmail.com>
2024-03-25 10:22:37 +0000

    inital display of all hats

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,5 +1,29 @@
-export default function Page() {
-  <div>
-    <h1>Display a mix of caps and other hats</h1>
-  </div>;
+import { sql } from "@vercel/postgres";
+import Image from "next/image";
+
+export default async function Page({ params }) {
+  const caps = (await sql`SELECT * FROM products`).rows;
+  console.log(params.products);
+
+  return (
+    <div>
+      <h1>Caps</h1>
+
+      <div>
+        {caps.map((cap) => (
+          <div key={cap.id}>
+            <h3>{cap.name}</h3>
+            <Image
+              className="object-cover rounded-md mb-2"
+              src={cap.image_url}
+              width={150}
+              height={150}
+              alt="image of cap"
+            />
+            <h2>Price: £{cap.price}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
