4405074b088539f0ded6d8bc16a7a4a81c811a15
gabaal <geraldbaalham@live.co.uk>
2024-03-25 18:39:03 +0000

    Gerald - SnipCart products from postgress, working on http://localhost:3000/snipcarttest

--- src/app/sqltests/page.js
+++ src/app/sqltests/page.js
@@ -1,20 +1,67 @@
+// import GetProducts from "../components/products-sql";
+// import GetProduct from "../components/products-sql";
+// // const productReviews = await GetProductReviews();
+// import { GetUsers } from "../components/users-sql";
+// // const products = await GetProducts();
+// // console.log(`PRODUCTS ${products}`);
+
+
+// export default async function productJSON() {
+//   const products = await GetProducts();
+//   console.log(products);
+  
+//   // const productsjson = JSON.stringify(products)
+//   // console.log(productsjson)
+  
+//   return(
+//     <div>
+//       <h1>Products</h1>
+//       <p>
+//         {products.rows.map((product) => (
+//           <li key={product.product_id}>
+//             <h2>{product.name}</h2>
+//             <p>{product.description}</p>
+//             <p>{product.image_url}</p>
+//             <p>{product.price}</p>
+//             <p>{product.stock}</p>
+//             <p>{product.category}</p>
+//           </li>
+//         ))}
+//       </p>
+      
+//     </div>
+//   )
+// }
+
 import GetProducts from "../components/products-sql";
-import GetProduct from "../components/products-sql";
-// const productReviews = await GetProductReviews();
-import { GetUsers } from "../components/users-sql";
-// const products = await GetProducts();
-// console.log(products);
+import fs from 'fs';
 
-const users = await GetUsers();
+export default async function productJSON() {
+  
+  
+  const products = await GetProducts();
+  console.log(products);
 
+    // // Map over products and rename keys
+    // const productsWithRenamedKeys = products.rows.map(product => ({
+    //   'data-item-id': product.product_id,
+    //   "data-item-name": product.name,
+    //   "data-item-description": product.description,
+    //   "data-item-image": product.image_url,
+    //   "data-item-price": product.price
+    // }));
+    // console.log(productsWithRenamedKeys);
+  
+  // Convert products data to JSON
+  const productsJSON = JSON.stringify(products.rows, null, 2);
+console.log(productsJSON);
+  // Write JSON data to a file
+fs.writeFileSync('src/app/SCproducts.json', productsJSON);
 
-
-export default async function Home() {
   return (
-    <main>
-      <div>
-        <h1>Hat-E home page</h1>
-      </div>
-    </main>
+    <div>
+      <h1>Products</h1>
+      <p>JSON file generated successfully!</p>
+    </div>
   );
 }
