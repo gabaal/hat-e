a8822513fc9afeb46cc780ae9add04b045bcc755
gabaal <geraldbaalham@live.co.uk>
2024-03-26 14:18:27 +0000

    About-us page done

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,4 +1,4 @@
-import products from "../products.json";
+import products from "../SCproducts.json";
 import Image from "next/image";
 export default function page() {
   console.log(products);
@@ -11,17 +11,19 @@ export default function page() {
             key={product.product_id}
             className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>${product.price}</p>
-            <Image src={product.image_url} height={400} width={300} />
+            <h3>{product.product_name}</h3>
+            <p>{product.product_description}</p>
+            <Image src={product.product_image_url} height={400} width={300} />
+            <p>${product.product_price}</p>
+            
+            <p>Avg Customer Review {product.average_rating}</p>
             <p>
               <button
                 className="snipcart-add-item"
                 data-item-id={product.product_id}
-                data-item-image={product.image_url}
-                data-item-name={product.name}
-                data-item-price={product.price}
+                data-item-image={product.product_image_url}
+                data-item-name={product.product_name}
+                data-item-price={product.product_price}
               >
                 Add to Cart
               </button>
