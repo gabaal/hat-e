3bd7600f9b6abf77004563d1edb73cc0966b2751
gabaal <geraldbaalham@live.co.uk>
2024-03-27 10:21:15 +0000

    still behind main

--- src/app/shop/[id]/page.js
+++ src/app/shop/[id]/page.js
@@ -1,30 +1,4 @@
-// export default async function Page({ params }) {
-//   console.log(params);
 
-//   const cap = (
-//     await sql`SELECT * FROM products WHERE product_id = ${params.id}`
-//   ).rows[0];
-
-//   return (
-//     <div key={cap.product_id}>
-//       <div>
-//         <h1>{cap.name}</h1>
-//       </div>
-//       {cap.image_url && (
-//         <Image
-//           className="object-cover rounded-md mb-2"
-//           src={cap.image_url}
-//           width={150}
-//           height={150}
-//           alt="image of cap"
-//         />
-//       )}
-
-//       <h3>Price: ${cap.price}</h3>
-//       <p>{cap.description}</p>
-//     </div>
-//   );
-// }
 import { sql } from "@vercel/postgres";
 import Image from "next/image";
 import productsArray from "../../SCproducts.json";
@@ -32,22 +6,12 @@ import { GetProductReviews } from "@/app/components/products-sql";
 
 
 export default async function Page({ params }) {
-  
-  // console.log"params.id);
-  // console.log(products);
-  // if (!product) {
-  //   return (
-  //     <div>
-  //       <p> No product found </p>
-  //     </div>
-  //   );
-  // }
-  //when product found
-  // const products = JSON.parse(productsArray);
+
   const postId = params.id;
   const product = productsArray.find(
     (product) => product.product_id === parseInt(postId)
   );
+  
   console.log(product);
   
     const reviews = await sql`
@@ -80,16 +44,17 @@ export default async function Page({ params }) {
         </button>
         </p>
       <div className="mt-4">
-        {reviews.length > 0 && (
-          <h3>Reviews</h3>
-        )}
-        <h3>Reviews</h3>
-        {reviews.rows.map((review) => (
-          <p key={review.review_id}>
-            {review.rating} - {review.comment}
-          </p>
-        ))}
-      </div>
+  {reviews.rows && reviews.rows.length > 0 && (
+    <>
+      <h3 className="text-lg font-semibold mb-2">Reviews</h3>
+      {reviews.rows.map((review) => (
+        <p key={review.review_id} className="text-lg mb-2 font-style: italic">
+          {review.rating} - {review.comment}
+        </p>
+      ))}
+    </>
+  )}
+</div>
     </div>
   );
 }
