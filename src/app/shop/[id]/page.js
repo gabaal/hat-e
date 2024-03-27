610c1cf613f48b180489053bafb9a08a696029cc
gabaal <geraldbaalham@live.co.uk>
2024-03-27 13:31:28 +0000

    sorting done plus starts

--- src/app/shop/[id]/page.js
+++ src/app/shop/[id]/page.js
@@ -1,49 +1,12 @@
-// export default async function Page({ params }) {
-//   console.log(params);
 
-//   const cap = (
-//     await sql`SELECT * FROM products WHERE product_id = ${params.id}`
-//   ).rows[0];
-
-//   return (
-//     <div key={cap.product_id}>
-//       <div>
-//         <h1>{cap.name}</h1>
-//       </div>
-//       {cap.image_url && (
-//         <Image
-//           className="object-cover rounded-md mb-2"
-//           src={cap.image_url}
-//           width={150}
-//           height={150}
-//           alt="image of cap"
-//         />
-//       )}
-
-//       <h3>Price: ${cap.price}</h3>
-//       <p>{cap.description}</p>
-//     </div>
-//   );
-// }
 import { sql } from "@vercel/postgres";
 import Image from "next/image";
 import productsArray from "../../SCproducts.json";
-import { GetProductReviews } from "@/app/components/products-sql";
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa'
 
 
 export default async function Page({ params }) {
-  
-  // console.log"params.id);
-  // console.log(products);
-  // if (!product) {
-  //   return (
-  //     <div>
-  //       <p> No product found </p>
-  //     </div>
-  //   );
-  // }
-  //when product found
-  // const products = JSON.parse(productsArray);
+
   const postId = params.id;
   const product = productsArray.find(
     (product) => product.product_id === parseInt(postId)
@@ -57,39 +20,49 @@ export default async function Page({ params }) {
     
     console.log(reviews);
     
-  return (
-    <div className="max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h1 className="text-2xl font-semibold">{product.product_name}</h1>
-      <p className="mt-2 text-m text-gray-600">{product.product_description}</p>
-      <Image className="mt-4"
-        src={product.product_image_url}
-        alt={product.product_name}
-        width={500}
-        height={100}
-      />
-      <p className="mt-4 text-lg font-bold text-gray-800">£{product.product_price}</p>
-      <p>
-        <button
-          className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300 snipcart-add-item"
-          data-item-id={product.product_id}
-          data-item-image={product.product_image_url}
-          data-item-name={product.product_name}
-          data-item-price={product.product_price}
-        >
-          Add to Cart
-        </button>
-        </p>
-      <div className="mt-4">
-        {reviews.length > 0 && (
-          <h3>Reviews</h3>
+    return (
+      <div className="max-w-xl mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h1 className="text-2xl font-semibold">{product.product_name}</h1>
+        <p className="mt-2 text-lg text-gray-600">{product.product_description}</p>
+        <Image className="mt-4" src={product.product_image_url} alt={product.product_name} width={500} height={100} />
+        <div className="mt-4 flex justify-between items-center">
+          <div>
+            <p className="text-lg font-bold text-gray-800">£{product.product_price}</p>
+          </div>
+          <button
+            className="text-sm font-semibold px-4 py-2 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 snipcart-add-item"
+            data-item-id={product.product_id}
+                  data-item-image={product.product_image_url}
+                  data-item-name={product.product_name}
+                  data-item-price={product.product_price}
+          >
+            Add to Cart
+          </button>
+        </div>
+        {reviews.rows.length > 0 && (
+          <div className="mt-4">
+            <h3 className="text-lg font-bold text-gray-800">Reviews</h3>
+            {reviews.rows.map((review) => (
+              <div key={review.review_id} className="flex items-center font-style: italic">
+                {Array.from({ length: Math.floor(review.rating) }, (_, index) => (
+                  <FaStar key={index} className="text-yellow-500" />
+                ))}
+                {review.rating % 1 !== 0 && <FaStarHalfAlt className="text-yellow-500" />}
+                <span className="ml-2">{review.comment}</span>
+              </div>
+            ))}
+            {product.average_rating !== "0.0" && (
+              <div className="flex items-center mt-2">
+                <p>Average Review:</p>
+                {Array.from({ length: Math.floor(product.average_rating) }, (_, index) => (
+                  <FaStar key={index} className="text-yellow-500 ml-2" />
+                ))}
+                {product.average_rating % 1 !== 0 && <FaStarHalfAlt className="text-yellow-500" />}
+                <span className="ml-2">{product.average_rating}</span>
+              </div>
+            )}
+          </div>
         )}
-        <h3>Reviews</h3>
-        {reviews.rows.map((review) => (
-          <p key={review.review_id}>
-            {review.rating} - {review.comment}
-          </p>
-        ))}
       </div>
-    </div>
-  );
+    );
 }
--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -4,74 +4,101 @@ import { useState } from 'react';
 import products from "../SCproducts.json";
 import Link from "next/link";
 import Image from "next/image";
+import { FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 export default function Page() {
   const [sortOption, setSortOption] = useState(null);
+  const [isDescending, setIsDescending] = useState(true);
 
   const sortedProducts = [...products].sort((a, b) => {
+    let comparison = 0;
     switch (sortOption) {
       case 'price':
-        return a.product_price - b.product_price;
+        comparison = a.product_price - b.product_price;
+        break;
       case 'name':
-        return a.product_name.localeCompare(b.product_name);
+        comparison = a.product_name.localeCompare(b.product_name);
+        break;
       case 'review':
-        return b.average_rating - a.average_rating;
+        comparison = b.average_rating - a.average_rating;
+        break;
       default:
-        return 0;
+        break;
     }
+    return isDescending ? comparison : -comparison;
   });
+
+  const handleSortOptionChange = (option) => {
+    if (sortOption === option) {
+      setIsDescending(!isDescending);
+    } else {
+      setIsDescending(true);
+      setSortOption(option);
+    }
+  };
 
   return (
     <div>
-      <div className="flex justify-center mt-4">
+      <div className="flex justify-center mt-4 mb-4 space-x-4">
         <button
-          className="mr-4 bg-gray-200 px-4 py-2 rounded-md"
-          onClick={() => setSortOption('price')}
+          className="text-sm font-semibold px-4 py-2 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400"
+          onClick={() => handleSortOptionChange('price')}
         >
           Sort by Price
         </button>
         <button
-          className="mr-4 bg-gray-200 px-4 py-2 rounded-md"
-          onClick={() => setSortOption('name')}
+          className="text-sm font-semibold px-4 py-2 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400"
+          onClick={() => handleSortOptionChange('name')}
         >
           Sort by Name
         </button>
         <button
-          className="bg-gray-200 px-4 py-2 rounded-md"
-          onClick={() => setSortOption('review')}
+          className="text-sm font-semibold px-4 py-2 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400"
+          onClick={() => handleSortOptionChange('review')}
         >
           Sort by Review
         </button>
       </div>
-      <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-8 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {sortedProducts.map((product) => (
           <div
             key={product.product_id}
             className="max-w-md mx-auto bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden hover:shadow-lg dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <div className="p-6">
-              <h3 className="text-lg font-semibold text-gray-800">
-                {product.product_name}
-              </h3>
-              <p className="mt-2 text-sm text-gray-600">
-                {product.product_description}
-              </p>
-              <Link href={`shop/${product.product_id}`}>
-                <img
-                  className="object-cover object-center h-70 w-70"
-                  src={product.product_image_url}
-                  alt={product.product_name}
-                />
-              </Link>
-              <p className="mt-4 text-lg font-bold text-gray-800">
-                £{product.product_price}
-              </p>
-              <div className="flex items-center mt-4">
-                <p className="mr-2">
-                  Avg Customer Review {product.average_rating}
+            <div className="p-6 flex flex-col justify-between h-full">
+              <div>
+                <h3 className="text-xl font-semibold text-gray-800">
+                  {product.product_name}
+                </h3>
+                <p className="mt-2 text-lg text-gray-600">
+                  {product.product_description}
                 </p>
+                <Link href={`shop/${product.product_id}`}>
+                  <img
+                    className="object-cover object-center h-70 w-70"
+                    src={product.product_image_url}
+                    alt={product.product_name}
+                  />
+                </Link>
+              </div>
+              <div className="flex justify-between items-center mt-4">
+                <div>
+                  <p className="mt-4 text-lg font-bold text-gray-800">
+                    £{product.product_price}
+                  </p>
+                  {product.average_rating !== "0.0" && (
+                    <div className="flex items-center mt-2">
+                      <p>Average Reviews:</p>
+                      {Array.from({ length: Math.floor(Number(product.average_rating)) }, (_, index) => (
+                        <FaStar key={index} className="text-yellow-500 ml-2" />
+                      ))}
+                      {product.average_rating % 1 !== 0 && <FaStarHalfAlt className="text-yellow-500" />}
+                      <span className="ml-2">{product.average_rating}</span>
+                    </div>
+                  )}
+                </div>
                 <button
-                  className="text-sm font-semibold px-3 py-1 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 snipcart-add-item"
+                  className="text-sm font-semibold px-4 py-2 bg-[#AAE292] rounded-full hover:bg-gray-300 focus:outline-none focus:ring focus:ring-gray-400 snipcart-add-item"
                   data-item-id={product.product_id}
                   data-item-image={product.product_image_url}
                   data-item-name={product.product_name}
@@ -86,4 +113,4 @@ export default function Page() {
       </div>
     </div>
   );
-}
+}  
