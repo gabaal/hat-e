a4f19361493ade93f5feef9d60a48ddff1dac2ef
gabaal <geraldbaalham@live.co.uk>
2024-03-27 11:18:14 +0000

    Only display avg review is there are reviews completed

--- src/app/shop/[id]/page.js
+++ src/app/shop/[id]/page.js
@@ -55,6 +55,7 @@ export default async function Page({ params }) {
     </>
   )}
 </div>
-    </div>
+
+</div>
   );
 }
--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -2,29 +2,30 @@ import products from "../SCproducts.json";
 import productJSON from "../components/update-products-json";
 import Link from "next/link";
 
-export default async function page() {
-  // await productJSON();
+export default async function Page() {
+  await productJSON();
   console.log(products);
+  
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       <h1 className="mt-4 text-3xl text-gray-800">The Hat-e Shop</h1>
       {products.map((product) => {
-        
         return (
-          <div
-            key={product.product_id}
-            className="max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-      >
-            
-            <h3 className="text-lg font-semibold">{product.product_name}</h3>
-            <p className="mt-2 text-m text-gray-600">{product.product_description}</p>
-            <Link href={`shop/${product.product_id}`}>
-            <img className="mt-4" src={product.product_image_url} height={400}  />
-            </Link>
-            
-            <p className="mt-4 text-lg font-bold text-gray-800">£{product.product_price}</p>
-            <p>Avg Customer Review {product.average_rating}</p>
-            <p>
+          <div key={product.product_id} className="max-w-sm mx-auto p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 flex flex-col justify-between">
+            <div>
+              <h3 className="text-lg font-semibold">{product.product_name}</h3>
+              <p className="mt-2 text-m text-gray-600">{product.product_description}</p>
+              <Link href={`shop/${product.product_id}`}>
+                <img className="mt-4" src={product.product_image_url} height={400} />
+              </Link>
+            </div>
+            <div className="flex flex-col justify-between mt-4">
+              <div>
+                <p className="text-lg font-bold text-gray-800">£{product.product_price}</p>
+                {product.average_rating !== '0.0' && (
+                  <p className="text-lg font-semibold">Avg Customer Review {product.average_rating}</p>
+                )}
+              </div>
               <button
                 className="hover:text-gray-900 hover:shadow-md rounded-md px-4 py-2 bg-[#AAE292] text-black hover:bg-gray-300 snipcart-add-item"
                 data-item-id={product.product_id}
@@ -34,11 +35,8 @@ export default async function page() {
               >
                 Add to Cart 
               </button>
-              <p>
-              </p>
-            </p>
+            </div>
           </div>
-          
         );
       })}
     </div>
