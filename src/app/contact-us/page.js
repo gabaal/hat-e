1d05c2cbe18ec0d8e345a2b59b85a42fca885663
Gerald <36296159+gabaal@users.noreply.github.com>
2024-03-28 10:14:50 +0000

    Update page.js

--- src/app/contact-us/page.js
+++ src/app/contact-us/page.js
@@ -4,7 +4,7 @@ import NewMessage from "../message/page";
 export default async function Page() {
   return (
     <div className="flex flex-col items-center">
-      <h1 className="text-2xl mb-4">Contact Us</h1>
+      
       <NewMessage />
     </div>
   );
